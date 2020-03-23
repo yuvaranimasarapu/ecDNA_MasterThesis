8ba2265376ed9fc7f5d95581243581ee9838cca6
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-03-23 15:21:47 +0100

    new script for bwa mapping

--- scripts/cnv.sh
+++ scripts/cnv.sh
@@ -1,5 +1,5 @@
 bin=1000
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -tree /disk1/yuvarani/results/mapping/sample_data/mapping_output_sort.bam
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -tree /disk1/yuvarani/results/mapping/sample_data/mapping_output.sort.bam
 /disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root DHG00004.root -genome hg19  -his bin -d /disk1/pengweixing/software/sc-atac/refdata-cellranger-atac-hg19-1.2.0/fasta/chromesome
 /disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root DHG00004.root -genome hg19 -stat bin
 /disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root DHG00004.root -genome hg19 -partition bin
--- scripts/map_sample_yuvi.sh
+++ scripts/map_sample_yuvi.sh
@@ -1,2 +1,2 @@
 mkdir /disk1/yuvarani/results/mapping/sample_data
-bwa mem -t 50 /disk1/yuvarani/data/ref_genome/genome.fa /disk1/yuvarani/data/sample_pair_reads/NR140324-9_DHG00011_H07CTALXX_L5_1.fq.gz /disk1/yuvarani/data/sample_pair_reads/NR140324-9_DHG00011_H07CTALXX_L5_2.fq.gz |samtools view -bS |samtools sort -@ 5 -m 5G - > /disk1/yuvarani/results/mapping/sample_data/mapping_output_sort.bam
+bwa mem -t 50 /disk1/yuvarani/data/ref_genome/genome.fa /disk1/yuvarani/data/sample_pair_reads/NR140324-9_DHG00011_H07CTALXX_L5_1.fq.gz /disk1/yuvarani/data/sample_pair_reads/NR140324-9_DHG00011_H07CTALXX_L5_2.fq.gz |samtools view -bS |samtools sort -@ 5 -m 5G - > /disk1/yuvarani/results/mapping/sample_data/mapping_output_new.sort.bam
