29f59df7eb192895efe9d113e139b0871e21c28c
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-03-23 16:13:48 +0100

    chromosome split file updated with new bam file name

--- scripts/chromosome_split.sh
+++ scripts/chromosome_split.sh
@@ -1,7 +1,7 @@
-samtools idxstats /disk1/yuvarani/results/mapping/sample_data/mapping_output_sort.bam | cut -f1 | grep -v '*' | \
+samtools idxstats /disk1/yuvarani/results/mapping/sample_data/mapping_output_new.sort.bam | cut -f1 | grep -v '*' | \
   while read CHR
   do
-  samtools view -h /disk1/yuvarani/results/mapping/sample_data/mapping_output_sort.bam $CHR | \
+  samtools view -h /disk1/yuvarani/results/mapping/sample_data/mapping_output_new.sort.bam $CHR | \
   samtools fastq \
     -1 ${CHR}.fa \
     -2 ${CHR}.fa \
--- scripts/cnv.sh
+++ scripts/cnv.sh
@@ -1,6 +1,6 @@
 bin=1000
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -tree /disk1/yuvarani/results/mapping/sample_data/mapping_output.sort.bam
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root DHG00004.root -genome hg19  -his bin -d /disk1/pengweixing/software/sc-atac/refdata-cellranger-atac-hg19-1.2.0/fasta/chromesome
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root DHG00004.root -genome hg19 -stat bin
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root DHG00004.root -genome hg19 -partition bin
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root DHG00004.root -genome hg19 -call bin > DHG00004.cnv
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -tree /disk1/yuvarani/results/mapping/sample_data/mapping_output_new.sort.bam
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19  -his bin -d /disk1/pengweixing/software/sc-atac/refdata-cellranger-atac-hg19-1.2.0/fasta/chromesome
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19 -stat bin
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19 -partition bin
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19 -call bin > sample_cnv_output.cnv
