4da44d9cd8c1b2d03e157800363a07ed18d7ca9d
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-03-24 11:24:21 +0100

    script for cnvnator

--- scripts/cnv_script.sh
+++ scripts/cnv_script.sh
@@ -1,6 +1,6 @@
 bin=1000
 /disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -tree /disk1/yuvarani/results/mapping/sample_data/mapping_output_new.sort.bam
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19  -his bin -d /disk1/pengweixing/software/sc-atac/refdata-cellranger-atac-hg19-1.2.0/fasta/chromesome
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19  -his bin -d /disk1/yuvarani/data/ref_genome/chromosome
 /disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19 -stat bin
 /disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19 -partition bin
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19 -call bin > sample_cnv_output.cnv
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root sample_cnv_output.root -genome hg19 -call bin > /disk1/yuvarani/results/cnv_output/sample_cnv_output.cnv
