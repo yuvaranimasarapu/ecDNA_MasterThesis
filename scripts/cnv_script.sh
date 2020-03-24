33d53af66d7a421d409b8b2ed7c3ee23d987ce60
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-03-24 13:39:58 +0100

    cnv script sync 2

--- scripts/cnv_script.sh
+++ scripts/cnv_script.sh
@@ -1,6 +1,6 @@
 bin=1000
 /disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -tree /disk1/yuvarani/results/mapping/sample_data/mapping_output_new.sort.bam
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19  -his 1000 -d /disk1/yuvarani/data/ref_genome/chromosome
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -stat 1000
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -partition 1000
-/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -call 1000 > /disk1/yuvarani/results/cnv_output/cnv_sample_output.cnv
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19  -his $bin -d /disk1/yuvarani/data/ref_genome/chromosome
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -stat $bin
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -partition $bin
+/disk1/pengweixing/software/cnvnator/CNVnator/cnvnator -root out.root -genome hg19 -call $bin > /disk1/yuvarani/results/cnv_output/cnv_sample_output.cnv
