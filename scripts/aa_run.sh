d9efdb966606cf44892ee5fa2cc32dac386a534f
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-03-30 19:05:40 +0200

    AA script updated

--- scripts/aa_run.sh
+++ scripts/aa_run.sh
@@ -1,4 +1,4 @@
 out=/disk1/yuvarani/results/AA_software/sample_1_AA
 bam_file=/disk1/yuvarani/results/mapping/sample_data/mapping_output_new.sort.bam
 samtools index $bam_file
-/home/huazhang/miniconda2/bin/python /disk1/huazhang/software/AmpliconArchitect/src/AmpliconArchitect.py --bed /disk1/yuvarani/results/cnv_output/.cnv.bed --bam $bam_file --out $out
+/home/huazhang/miniconda2/bin/python /disk1/huazhang/software/AmpliconArchitect/src/AmpliconArchitect.py --bed /disk1/yuvarani/results/cnv_output/cnv_sample_output.cnv.bed --bam $bam_file --out $out
