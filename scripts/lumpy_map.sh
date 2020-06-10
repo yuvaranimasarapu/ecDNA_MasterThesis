a18762700437d0f98920b146d4e4e8dc177010d2
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-06-10 13:58:57 +0000

    bwa script changes

--- scripts/lumpy_map.sh
+++ scripts/lumpy_map.sh
@@ -1,8 +1,8 @@
 # Pre-processing before running LUMPY
 bwa mem -t 50 /disk1/yuvarani/data/ref_genome/genome.fa /disk1/yuvarani/data/sample_pair_reads/NR140324-9_DHG00011_H07CTALXX_L5_1.fq.gz /disk1/yuvarani/data/sample_pair_reads/NR140324-9_DHG00011_H07CTALXX_L5_2.fq.gz \
-	| /disk1/yuvarani/softwares/samblaster --excludeDups --addMateTags --maxSplitCount 2 --minNonOverlap 20 \
+	| samblaster --excludeDups --addMateTags --maxSplitCount 2 --minNonOverlap 20 \
 	| samtools view -S -b - \
-    > /disk1/yuvarani/results/cnv_testing/lumpy_sample.bam
+    	> /disk1/yuvarani/results/cnv_testing/lumpy_sample.bam
 
 # Extract discordant paired-end alignments
 samtools view -b -F 1294 /disk1/yuvarani/results/cnv_testing/lumpy_sample.bam > /disk1/yuvarani/results/cnv_testing/lumpy_sample.discordants.unsorted.bam
