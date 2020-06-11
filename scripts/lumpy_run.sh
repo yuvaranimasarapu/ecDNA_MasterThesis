038efb0152f0d8ee51975cdb7433bd62f35aad65
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-06-11 16:00:01 +0200

    lumpy_run first step testing

---
+++ scripts/lumpy_run.sh
@@ -0,0 +1,8 @@
+#empirical insert size of each library in bam 
+samtools view -r readgroup1 /disk1/yuvarani/results/cnv_testing/lumpy_sample.sort.bam \
+    | tail -n+100000 \
+    | disk1/yuvarani/softwares/cnv_softwares/lumpy-sv/scripts/pairend_distro.py \
+    -r 101 \
+    -X 4 \
+    -N 10000 \
+    -o lumpy_sample.lib1.histo
