d724041dcdbbd403c3ae0608677f43c0eed75000
yuvaranimasarapu <yuvaranimasarapu@gmail.com>
2020-06-11 16:01:27 +0200

    C:/Program Files/Git/ adjusted for read

--- scripts/lumpy_run.sh
+++ scripts/lumpy_run.sh
@@ -1,7 +1,7 @@
 #empirical insert size of each library in bam 
 samtools view -r readgroup1 /disk1/yuvarani/results/cnv_testing/lumpy_sample.sort.bam \
     | tail -n+100000 \
-    | disk1/yuvarani/softwares/cnv_softwares/lumpy-sv/scripts/pairend_distro.py \
+    | /disk1/yuvarani/softwares/cnv_softwares/lumpy-sv/scripts/pairend_distro.py \
     -r 101 \
     -X 4 \
     -N 10000 \
